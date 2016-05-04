e81efc0a0639241972f7bff756ba28a3d59e85f1
Chenjie Lu <lcjfly@gmail.com>
2016-05-04 13:35:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ console.log(process.env);
 const server = http.createServer(function (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World2\n');
 });
 
 server.listen(port, hostname, function () {
