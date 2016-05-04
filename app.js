12c81dc6c05f85697f7699ccf7d02106205273ad
Chenjie Lu <lcjfly@gmail.com>
2016-05-04 13:33:47 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = process.env.IP;
+const port = process.env.PORT || '3000';
+console.log(process.env);
+
+const server = http.createServer(function (req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, function () {
+  console.log('Server running at http://'+hostname+':'+port);
+});
